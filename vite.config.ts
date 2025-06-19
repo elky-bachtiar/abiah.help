a7d7b64bdb7b2aac3ba64bd8ab8ce4087613148a
Elky Bachtiar <139325226+elky-bachtiar@users.noreply.github.com>
2025-06-19 19:31:27 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Resource-Policy': 'cross-origin'
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: function (file) {
+          return file.name?.endsWith(".mp3")
+            ? `assets/[name].[ext]`
+            : `assets/[name]-[hash].[ext]`;
+        },
+      },
+    },
   },
 });
