ced1ab4572f92862abff5b8b07ba247c81540020
Elky Bachtiar <139325226+elky-bachtiar@users.noreply.github.com>
2025-06-24 11:20:30 +0200

    Updated PricingCard.tsx

--- src/components/pricing/PricingCard.tsx
+++ src/components/pricing/PricingCard.tsx
@@ -105,7 +105,6 @@ export function PricingCard({
           )}
         </div>
       </div>
-    </motion.div>
       <div className="p-6 flex-grow">
         <ul className="space-y-3 mb-6">
           {features.map((feature, index) => (
