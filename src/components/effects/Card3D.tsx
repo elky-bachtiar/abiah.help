8885de7fae9175830ee9fdf79843c79aeaf47fc6
Elky Bachtiar <139325226+elky-bachtiar@users.noreply.github.com>
2025-06-29 21:15:38 +0200

    Updated Card3D.tsx

--- src/components/effects/Card3D.tsx
+++ src/components/effects/Card3D.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { motion } from 'framer-motion';
-import { cn } from '@/lib/utils';
+import { cn } from '../../lib/utils';
 
 interface Card3DProps {
   children: React.ReactNode;
